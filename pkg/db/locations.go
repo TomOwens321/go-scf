6440e2f2cb276528c51110b8fd1c2b8f350b76fa
TomOwens321 <tomowens321@gmail.com>
2023-09-04 10:42:12 -0600

    First draft of a locations table (#10)
    * First draft of a locations table
    
    * Create a test location and assign it to a plant
    
    * Remove unused config
    
    ---------
    
    Co-authored-by: Tom Owens <tom@towens.com>

---
+++ pkg/db/locations.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+type Location struct {
+	gorm.Model
+	Name        string `gorm:"unique;index"`
+	Description string
+	Plants      []Plant `gorm:"many2many:plant_locations;"`
+	Lattitude   float64
+	Longitute   float64
+	Altitude    int
+	State       string
+	County      string
+	City        string
+}
+
+func init() {
+	db := GetDB()
+	db.AutoMigrate(&Location{})
+}
+
+func AllLocations() ([]Location, error) {
+	var locations []Location
+	err := db.Preload(clause.Associations).Find(&locations)
+	return locations, err.Error
+}
+
+func GetLocation(id uint) (Location, error) {
+	var location Location
+	err := db.Preload(clause.Associations).Find(&location, id)
+	return location, err.Error
+}
+
+func GetLocationByName(name string) (Location, error) {
+	var location Location
+	err := db.Preload(clause.Associations).Where("name = ?", name).First(&location)
+	return location, err.Error
+}

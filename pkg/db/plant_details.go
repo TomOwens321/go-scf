aabe0176376163933067e39c4f39970ae5731d7b
TomOwens321 <tomowens321@gmail.com>
2024-04-27 07:37:46 -0600

    Start to build up the UI a bit more (#11)
    * Start to build up the UI a bit more
    
    * Redefine dev container
    
    * Add a few more plant detail columns
    
    * Enable FullSaveAssociations to allow editing description
    Edit function properly identifies a plant
    Add fields to show page and edit form
    
    ---------
    
    Co-authored-by: Tom Owens <tom@towens.com>

--- pkg/db/plant_details.go
+++ pkg/db/plant_details.go
@@ -7,8 +7,14 @@ import (
 type PlantDetail struct {
 	gorm.Model
 	PlantID     uint
+	Altitude    uint
+	Climate     string
 	Description string
-	Altitude    string
+	FlowerColor string
+	FlowerTime  string
+	Hardiness   string
+	Height      float32
+	Spread      float32
 	Zone        uint
 }
 
